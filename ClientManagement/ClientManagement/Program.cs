992711c6cfee68feec34ff6a6535e7ff39b29c7a
kiran kumar <kirank100694@gmail.com>
2024-08-09 17:14:51 +0530

    Commit

--- ClientManagement/ClientManagement/Program.cs
+++ ClientManagement/ClientManagement/Program.cs
@@ -9,6 +9,7 @@ namespace ClientManagement
 
             // Add services to the container.
 
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
